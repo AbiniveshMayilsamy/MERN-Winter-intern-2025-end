3b7ec11acd79ee086c4955c63d9af79b09402458
Abinivesh Mayilsamy <abiniveshmayilsamy1@gmail.com>
2025-12-04 12:43:31 +0530

    filter cmd

--- indexjs.js
+++ indexjs.js
@@ -1,109 +1,117 @@
-// console.log(a);
-// //hoisting
-// var a = 10;
-// // global scope
-// console.log(a);
-// //ES6
-// //let and const
-// //reference error
-// // temporal dead zone
-// let b = 20;
-// //console.log(b);
-// b = 130;
-// //block scope
-// console.log(b);
-// console.log(c);
-// const c=40;
-// c=70;
+// // console.log(a);
+// // //hoisting
+// // var a = 10;
+// // // global scope
+// // console.log(a);
+// // //ES6
+// // //let and const
+// // //reference error
+// // // temporal dead zone
+// // let b = 20;
+// // //console.log(b);
+// // b = 130;
+// // //block scope
+// // console.log(b);
+// // console.log(c);
+// // const c=40;
+// // c=70;
 
-// <----JavaScript Functions------->
-//1.Named Function
-// function namedFun() {
-//   console.log("Named Function called");
-//   console.log("Named Function called");
-//   console.log("Named Function called");
+// // <----JavaScript Functions------->
+// //1.Named Function
+// // function namedFun() {
+// //   console.log("Named Function called");
+// //   console.log("Named Function called");
+// //   console.log("Named Function called");
+// // }
+// //-----------------------
+// // namedFun();
+// //2.Function Expression
+// // let funcExp = function () {
+// //   console.log("Function Expression called");
+// // };
+
+// // // funcExp();
+
+// // //----------------------
+// // //3.Arrow Expression
+// // // let arrrow = () => {
+// // //   console.log("Arrow Expression called");
+// // // };
+// // // arrrow();
+// // //---------------------
+// // // //4.callback function and IFFE
+// // // (() => {
+// // //   console.log("callback and IFFE function called");
+// // // })();
+// // // const addTwonumbers = (parameter1, parameter2) => {
+// // //   let num1 = parameter1;
+// // //   let num2 = parameter2;
+// // //   let sum = num1 + num2;
+// // //   console.log("The sum of two number is:", sum);
+// // // };
+
+// // // addTwonumbers(20, 50);
+// // // addTwonumbers(40, 50);
+// // // addTwonumbers();
+
+// // // const subTwonumbers = (parameter1, parameter2) => {
+// // //   let num1 = parameter1;
+// // //   let num2 = parameter2;
+// // //   let sum = num1 - num2;
+// // //   console.log(`The sub of two number is:${sum}`, sum);
+// // // };
+// // // subTwonumbers(20, 50);
+// // // subTwonumbers(40, 50);
+
+// // var k = 10;
+// // console.log(typeof k);
+
+// // const MultiTwonumbers = (parameter1, parameter2) => {
+// //   let num1 = parameter1;
+// //   let num2 = parameter2;
+// //   let sum = num1 * num2;
+// //   console.log(`The sub of two number is:${sum}`);
+// // };
+// // MultiTwonumbers(20, 50);
+// // MultiTwonumbers(40, 50);
+
+// // const DivTwonumbers = (parameter1, parameter2) => {
+// //   let num1 = parameter1;
+// //   let num2 = parameter2;
+// //   let sum = num1 / num2;
+// //   console.log(`The sub of two number is:${sum}`);
+// // };
+// // DivTwonumbers(20, 50);
+// // DivTwonumbers(40, 50);
+
+// // let a = "10";
+// // let b = 20;
+// // console.log(a + b);
+
+// // let c = [1, 2, 3];
+// // let d = [1, 2, 3];
+// // console.log(c[(0, 1, 2)] == d[(0, 1, 2)]);
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// //forloop
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
 // }
-//-----------------------
-// namedFun();
-//2.Function Expression
-// let funcExp = function () {
-//   console.log("Function Expression called");
-// };
 
-// // funcExp();
+// //foreach
+// arr.forEach((element, index) => {
+//   console.log(element, index);
+// });
 
-// //----------------------
-// //3.Arrow Expression
-// // let arrrow = () => {
-// //   console.log("Arrow Expression called");
-// // };
-// // arrrow();
-// //---------------------
-// // //4.callback function and IFFE
-// // (() => {
-// //   console.log("callback and IFFE function called");
-// // })();
-// // const addTwonumbers = (parameter1, parameter2) => {
-// //   let num1 = parameter1;
-// //   let num2 = parameter2;
-// //   let sum = num1 + num2;
-// //   console.log("The sum of two number is:", sum);
-// // };
+// //map
+// arr.map((element, index) => {
+//   console.log(element, index);
+// });
 
-// // addTwonumbers(20, 50);
-// // addTwonumbers(40, 50);
-// // addTwonumbers();
+let a = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-// // const subTwonumbers = (parameter1, parameter2) => {
-// //   let num1 = parameter1;
-// //   let num2 = parameter2;
-// //   let sum = num1 - num2;
-// //   console.log(`The sub of two number is:${sum}`, sum);
-// // };
-// // subTwonumbers(20, 50);
-// // subTwonumbers(40, 50);
-
-// var k = 10;
-// console.log(typeof k);
-
-// const MultiTwonumbers = (parameter1, parameter2) => {
-//   let num1 = parameter1;
-//   let num2 = parameter2;
-//   let sum = num1 * num2;
-//   console.log(`The sub of two number is:${sum}`);
-// };
-// MultiTwonumbers(20, 50);
-// MultiTwonumbers(40, 50);
-
-// const DivTwonumbers = (parameter1, parameter2) => {
-//   let num1 = parameter1;
-//   let num2 = parameter2;
-//   let sum = num1 / num2;
-//   console.log(`The sub of two number is:${sum}`);
-// };
-// DivTwonumbers(20, 50);
-// DivTwonumbers(40, 50);
-
-// let a = "10";
-// let b = 20;
-// console.log(a + b);
-
-// let c = [1, 2, 3];
-// let d = [1, 2, 3];
-// console.log(c[(0, 1, 2)] == d[(0, 1, 2)]);
-
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-//forloop
-for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
-}
-
-//foreach
-arr.forEach((element, index) => {
-  console.log(element, index);
+// filter: it will work on Truthy Values
+let b = a.filter((element, index) => {
+  return index > 4;
 });
-
-//map
-arr.map((element, index) => {
-  console.log(element, index);
-});
+console.log(b);
