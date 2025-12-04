57aa7ede19ba53710de18b8bc60d4d3a7a7c3e48
Abinivesh Mayilsamy <abiniveshmayilsamy1@gmail.com>
2025-12-04 10:28:43 +0530

    Named Function calling

--- indexjs.js
+++ indexjs.js
@@ -1,11 +1,27 @@
-console.log(a);
-//hoisting
-var a = 10;
-// global scope
-console.log(a);
-//ES6
-//let and const
-//reference error
-// temporal dead zone
-let b = 20;
-console.log(b);
+// console.log(a);
+// //hoisting
+// var a = 10;
+// // global scope
+// console.log(a);
+// //ES6
+// //let and const
+// //reference error
+// // temporal dead zone
+// let b = 20;
+// //console.log(b);
+// b = 130;
+// //block scope
+// console.log(b);
+// console.log(c);
+// const c=40;
+// c=70;
+
+// <----JavaScript Functions------->
+//1.Named Function
+function namedFun() {
+  console.log("Named Function called");
+  console.log("Named Function called");
+  console.log("Named Function called");
+}
+
+namedFun();
