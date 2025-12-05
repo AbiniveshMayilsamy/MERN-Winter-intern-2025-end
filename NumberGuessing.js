530fd81e78aba5004afc897e832c249c5963d36d
Abinivesh Mayilsamy <abiniveshmayilsamy1@gmail.com>
2025-12-05 10:37:45 +0530

    adding js to Nummber Guessing Website

---
+++ NumberGuessing.js
@@ -0,0 +1,54 @@
+// 1 We need all the instance
+let againBtn = document.querySelector(".again");
+let number = document.querySelector(".number");
+let guess = document.querySelector(".guess");
+let checkBtn = document.querySelector(".check");
+let score = document.querySelector(".score");
+let highScore = document.querySelector(".highscore");
+let msg = document.querySelector(".message");
+
+// 2. Generate a Rnadom Number betwenn 1 and 20
+let randomNumber = Math.trunc(Math.random() * 20) + 1;
+
+// 2.1 creat a varible of score
+let scr = 20;
+
+// Button Functionalities
+// 1.event  that we want to occur 2. Callback
+checkBtn.addEventListener("click", () => {
+  console.log("Button Clicked", randomNumber);
+  console.log(randomNumber);
+});
+
+// check the button functionality
+checkBtn.addEventListener("click", () => {
+  let guessNumber = Number(guess.value);
+  console.log(guessNumber, typeof guessNumber);
+
+  // when there is no input
+  if (!guessNumber) {
+    msg.textContent = "⛔ No Number!";
+  }
+  // when player wins
+  else if (guessNumber === randomNumber) {
+    msg.textContent = "🎉 Correct Number!";
+    number.textContent = randomNumber;
+    document.body.style.backgroundColor = "#60b347";
+    number.style.width = "30rem";
+    if (scr > highScore.textContent) {
+      highScore.textContent = scr;
+    }
+  }
+  // when guess is wrong
+  else if (guessNumber !== randomNumber) {
+    if (scr > 1) {
+      msg.textContent =
+        guessNumber > randomNumber ? "📈 Too High!" : "📉 Too Low!";
+      scr--;
+      score.textContent = scr;
+    } else {
+      msg.textContent = "💥 You lost the game!";
+      score.textContent = 0;
+    }
+  }
+});
