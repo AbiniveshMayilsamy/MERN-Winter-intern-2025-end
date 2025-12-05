879b8db2a0e80d48b64779882c41390b761ba366
Abinivesh Mayilsamy <abiniveshmayilsamy1@gmail.com>
2025-12-05 09:24:01 +0530

    DOM

---
+++ dom.js
@@ -0,0 +1,20 @@
+//1 Tag Selector
+let a = document.getElementsByTagName("h1");
+console.log(a);
+
+//2. class Selector
+let cl = document.getElementsByClassName("a");
+console.log(cl);
+
+//3. ID Selector
+let id = document.getElementById("name");
+console.log(id);
+
+//4.Query Selector
+let q = document.querySelector("h1");
+// it will return single
+console.log(q);
+
+//5. Query Selector all
+let q1 = document.querySelectorAll("#name");
+console.log(q1);
