98f4db2a654601612fad789fa2595233fa434f9d
Abinivesh Mayilsamy <abiniveshmayilsamy1@gmail.com>
2025-12-05 09:52:11 +0530

    add

--- dom.js
+++ dom.js
@@ -1,39 +1,46 @@
-// //1 Tag Selector
-// let a = document.getElementsByTagName("h1");
-// console.log(a);
+// // //1 Tag Selector
+// // let a = document.getElementsByTagName("h1");
+// // console.log(a);
 
-// //2. class Selector
-// let cl = document.getElementsByClassName("a");
-// console.log(cl);
+// // //2. class Selector
+// // let cl = document.getElementsByClassName("a");
+// // console.log(cl);
 
-// //3. ID Selector
-// let id = document.getElementById("b");
-// console.log(id);
+// // //3. ID Selector
+// // let id = document.getElementById("b");
+// // console.log(id);
 
-// //4.Query Selector
-// let q = document.querySelector("h1");
-// // it will return single
-// console.log(q);
+// // //4.Query Selector
+// // let q = document.querySelector("h1");
+// // // it will return single
+// // console.log(q);
 
-// //5. Query Selector all
-// let q1 = document.querySelectorAll("#name");
-// console.log(q1);
+// // //5. Query Selector all
+// // let q1 = document.querySelectorAll("#name");
+// // console.log(q1);
 
-//    <-----------READ AND WRITE OPERATION--------------------->
+// //    <-----------READ AND WRITE OPERATION--------------------->
 
-let head = document.querySelector("p");
-// Read OPeration
-console.log(head.textContent);
+// let head = document.querySelector("p");
+// // Read OPeration
+// console.log(head.textContent);
 
-// Write Operation
-head.textContent = "This is new Para text content.";
-console.log(head.textContent);
+// // Write Operation
+// head.textContent = "This is new Para text content.";
+// console.log(head.textContent);
 
-let div = document.querySelector("div");
-div.innerHTML = "<h1> Hello From Para </h1>";
-//div.textContent = "<h1> Hello From Para </h1>";
-console.log(div.textContent);
-console.log(div.innerHTML);
+// let div = document.querySelector("div");
+// div.innerHTML = "<h1> Hello From Para </h1>";
+// //div.textContent = "<h1> Hello From Para </h1>";
+// console.log(div.textContent);
+// console.log(div.innerHTML);
 
-let div1 = document.querySelector("div");
-div1.innerHTML = "<h1> hello from inner Html</h1>";
+// let div1 = document.querySelector("div");
+// div1.innerHTML = "<h1> hello from inner Html</h1>";
+
+// add/ remove/ toggle class
+let head = document.querySelector("h1");
+console.log(head);
+
+head.classList.add("color");
+head.classList.add("background");
