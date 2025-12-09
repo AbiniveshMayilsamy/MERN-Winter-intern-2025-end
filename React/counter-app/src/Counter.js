aaefc044fa225d2a322c9833104c4fb88aeb2ff6
Abinivesh Mayilsamy <abiniveshmayilsamy1@gmail.com>
2025-12-09 12:09:43 +0530

    React - counter-decrement - negative check

--- React/counter-app/src/Counter.js
+++ React/counter-app/src/Counter.js
@@ -5,6 +5,11 @@ const Counter = ({ val, handleIncrement, handleDecrement }) => {
     <div className="Counter">
       <h1>Counter</h1>
       <h1>{val}</h1>
+      {val < 0 ? (
+        <h1 style={{ backgroundColor: "white", color: "red" }}>
+          Negative Value
+        </h1>
+      ) : null}
       <ButtonContainer
         handleIncrement={handleIncrement}
         handleDecrement={handleDecrement}
