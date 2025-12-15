37385181f4255438c9bcb820880e0c4cf746ad70
Abinivesh Mayilsamy <abiniveshmayilsamy1@gmail.com>
2025-12-15 10:16:08 +0530

    *--- EXPRESS-JSON-https-ID.Writefile ---*

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const fs = require("fs");
 const app = express();
+app.use(express.json());
+
+// read json file
 
 const jsonData = JSON.parse(fs.readFileSync("abc.json", "utf-8"));
 console.log(jsonData);
@@ -43,6 +46,24 @@ app.get("/api/v1/abc/single/:id", (req, res) => {
     data: {
       singleData,
     },
+  });
+});
+
+app.post("/api/v1/abc/", (req, res) => {
+  const id = jsonData.length;
+  const newData = Object.assign({ id: id + 1 }, req.body);
+  jsonData.push(newData);
+  fs.writeFile("abc.json", JSON.stringify(jsonData), "utf-8", (err) => {
+    if (err) {
+      res.status(500).json({
+        status: "error , not to write file ",
+      });
+    } else {
+      res.status(201).json({
+        status: "success",
+        data: newData,
+      });
+    }
   });
 });
 
